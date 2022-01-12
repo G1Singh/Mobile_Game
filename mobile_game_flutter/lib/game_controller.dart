cfb5dd4c70c467cb770f700c8ce759c2010d4322
G1Singh <gurnaaz.singh@gmail.com>
2022-01-12 18:15:09 +0530

    Second Commit

---
+++ mobile_game_flutter/lib/game_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flame/game.dart';
+import 'dart:ui';
+// import 'package:flutter/cupertino.dart';
+
+class GameController extends Game {
+  GameController() {}
+
+  void render(Canvas c) {}
+
+  void update(double t) {}
+
+  void resize(Size size) {}
+}
